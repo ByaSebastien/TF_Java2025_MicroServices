b7ad49937bfb3861624c21807a3456904b8599ac
Bya Sebastien <byaebastien@hotmail.com>
2025-10-27 14:50:46 +0100

    not funcional

--- payment-service/src/main/java/be/bstorm/paymentservice/clients/OrderClient.java
+++ payment-service/src/main/java/be/bstorm/paymentservice/clients/OrderClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("order-service")
+@FeignClient(name = "order-service")
 public interface OrderClient {
 
     @GetMapping("/orders/{orderId}")
